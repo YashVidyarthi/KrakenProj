4232b2ba9ee35a74231c8d32bbcd79afa3d96bdf
Yash Vidyarthi <10018568@sbstudents.org>
2019-06-07 08:52:53 -0400

    Runner and interface for perfect square functionality created

---
+++ PerfectSquareRunner.java
@@ -0,0 +1,12 @@
+public class PerfectSquareRunner{
+	public static void main(String[]args){
+		PerfectSquare ps = (int perfectSquare) -> {
+			for(int x = 0;x<perfectSquare;x++){
+				if(x*x == perfectSquare)
+					return true;
+		}
+		return false;
+	};
+	System.out.println(ps.perfectSquare(25));
+	}
+}
